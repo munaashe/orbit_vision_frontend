34adb3bb3e648563694806c2e332fe91a007e82c
munaashe <denis.siduna@gmail.com>
2024-04-18 16:48:09 +0200

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: 'USDZ Image Rendere',
+  description: 'Using Open3d on a Python Backend',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
