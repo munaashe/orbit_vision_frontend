252784c114672faae335cb34d84e9d8826833d7a
munaashe <denis.siduna@gmail.com>
2024-04-27 23:20:12 +0200

    add tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: '#6CbC5C',
+        secondary: '#3F4A3C',
+        tertiary1: '#A3AF9E',
+        tertiary2: '#00C0F7',
+      },
+      screens: {
+        'xs': { 'max': '639px' },
+        'sm': { 'min': '640px', 'max': '767px' },
+        'md': { 'min': '768px', 'max': '1023px' },
+        'lg': { 'min': '1024px' },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
