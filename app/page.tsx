9aab9bd2aa9d58e77a060b69776ca7740458b4c6
munaashe <denis.siduna@gmail.com>
2024-05-09 07:18:45 +0200

    load shopify products

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,24 @@
 import PlanInfo from "@/containers/PlanInfo";
 import ProductList from "@/containers/ProductList";
 import Summary from "@/containers/Summary";
-import { products } from "@/data/products";
 import { unstable_noStore as noStore } from 'next/cache'
-import CryptoJS from 'crypto-js';
-
-interface AccountDetails {
-  shop_name: string;
-  access_token: string;
-}
-const initialState: AccountDetails = {
-  shop_name: '',
-  access_token: '',
-}
 
 
 export default async function Home() {
   noStore()
-  const secretKey: any = process.env.ENCRYPT_SECRET_KEY
 
-  const baseURL: any = process.env.BASE_API_URL
+  const baseURL: any = process.env.NEXT_PUBLIC_BASE_API_URL
   async function getAccountDetails() {
-    const res = await fetch(`${baseURL}/api/v1/account`)
+    const res = await fetch(`${baseURL}/account`)
+    if (!res.ok) {
+      throw new Error('Failed to fetch data')
+    }
+
+    return res.json()
+  }
+
+  async function getProducts() {
+    const res = await fetch(`${baseURL}/products`)
     if (!res.ok) {
       throw new Error('Failed to fetch data')
     }
@@ -30,14 +27,8 @@ export default async function Home() {
   }
 
   const accountData = await getAccountDetails()
-  console.log(accountData);
-
-
-  const plaintext = 'munopeeenga';
-
-  const ciphertext = CryptoJS.AES.encrypt(plaintext, secretKey).toString();
-  const decryptedText = CryptoJS.AES.decrypt(ciphertext, secretKey).toString(CryptoJS.enc.Utf8);
- 
+  const products = await getProducts()
+  console.log(products)
   return (
     <div className=" p-4 w-full bg-[#e1e1e1]">
       <div className="flex flex-grow items-start justify-center gap-4 w-full">
@@ -46,7 +37,10 @@ export default async function Home() {
           availableCredit='$200'
           productsQuantity='125'
         />
-        <PlanInfo />
+        <PlanInfo
+          shopName={accountData[0]?.shop_name}
+          accessToken={accountData[0]?.access_token}
+        />
       </div>
       <ProductList
         products={products}
--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -4,11 +4,10 @@ import denis from '@/assets/denis.jpg'
 
 interface Product {
   id: number;
-  title: string;
-  description?: string;
+  name: string;
   price: number;
-  imageUrl?: string;
-  availableQuantity?: number;
+  images: string[];
+  stock?: number;
 }
 
 interface ProductCardProps {
@@ -18,18 +17,18 @@ interface ProductCardProps {
 const ProductCard = ({ product }: ProductCardProps) => {
   return (
     <div className="product-card p-2">
-      <div className='h-[110px]'>
+      <div className='h-[110px] w-full flex items-center justify-center'>
         <Image
-          src={denis}
-          alt={product.title}
+          src={product.images[0]}
+          alt={product.name}
           height={100}
           width={100}
         />
       </div>
-      <h3 className='text-[14px] font-semibold'>{product.title}</h3>
+      <h3 className='text-[14px] font-semibold'>{product.name.substring(0, 20)}</h3>
       <div className='text-[12px] text-secondary italic'>
-        {product.availableQuantity && (
-          <span className="product-quantity">{product.availableQuantity} in stock</span>
+        {product.stock && (
+          <span className="product-quantity">{product.stock} in stock</span>
         )}
       </div>
     </div>
